c5db544a7a89dae829fd8975e4e19039f965fb93
davdtheemonk <parishniclaus@gmail.com>
2023-01-14 00:55:37 +0300

    ff

--- src/Analytics/index.js
+++ src/Analytics/index.js
@@ -1,168 +1,178 @@
-import React from "react"
-import "./styles.css"
-import ContentHeader from "../ContentHeader"
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import 'chart.js/auto';
-import {Bar,Doughnut} from "react-chartjs-2"
+import React from "react";
+import "./styles.css";
+import ContentHeader from "../ContentHeader";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import "chart.js/auto";
+import { Bar, Doughnut } from "react-chartjs-2";
 import Sidebarprofile from "../Sidebarprofile";
 
-
-export default function Analytics( sidebarprofile,setSidebarprofile){
-    const state={
-        labels:[
-            'quiz','presentations','extra'
+export default function Analytics(sidebarprofile, setSidebarprofile) {
+  const state = {
+    labels: ["quiz", "presentations", "extra"],
+    datasets: [
+      {
+        label: "Students",
+        backgroundColor: [
+          "#B21F00",
+          "#C9D300",
+          "#2FDE00",
+          "#00A6B4",
+          "#6800B4",
         ],
-        datasets:[
-            {
-                label:'Students',
-                backgroundColor:[
-                    '#B21F00',
-                    '#C9D300',
-                    '#2FDE00',
-                    '#00A6B4',
-                    '#6800B4'
-                ],
-                hoverBackgroundColor:[
-                    '#501800',
-                    '#4b5000',
-                    '#175000',
-                    '#003350',
-                    '#35014F'
-                ],
-                data:[0,0,0,0]
-            }
-
-        ]
-    }
-    const months={
-        labels:[
-            'Jan','Feb','March','April','May','June','July','Aug','Sep','Oct','Nov','Dec'
+        hoverBackgroundColor: [
+          "#501800",
+          "#4b5000",
+          "#175000",
+          "#003350",
+          "#35014F",
         ],
-        datasets:[
-            {
-                label:'Analytics',
-                backgroundColor:[
-                    '#B21F00',
-                    '#C9D300',
-                    '#2FDE00',
-                    '#00A6B4',
-                    '#6800B4'
-                ],
-                hoverBackgroundColor:[
-                    '#501800',
-                    '#4b5000',
-                    '#175000',
-                    '#003350',
-                    '#35014F'
-                ],
-                data:[0,0,0,0]
-            }
+        data: [0, 0, 0, 0],
+      },
+    ],
+  };
+  const months = {
+    labels: [
+      "Jan",
+      "Feb",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Analytics",
+        backgroundColor: [
+          "#B21F00",
+          "#C9D300",
+          "#2FDE00",
+          "#00A6B4",
+          "#6800B4",
+        ],
+        hoverBackgroundColor: [
+          "#501800",
+          "#4b5000",
+          "#175000",
+          "#003350",
+          "#35014F",
+        ],
+        data: [0, 0, 0, 0],
+      },
+    ],
+  };
+  return (
+    <div className="analytics-cont-outer">
+      <div
+        style={{
+          backgroundColor: "rgb(122, 122, 243)",
+          position: "absolute",
+          bottom: "0px",
+          height: "10px",
+          left: "0px",
+          width: "100%",
+        }}
+      ></div>
 
-        ]
-    }
-    return(
-        <div className="analytics-cont-outer">
-        <div style={{backgroundColor:'rgb(122, 122, 243)',position:'absolute',bottom:"0px",height:"10px",left:'0px',width:"100%"}}></div>
-
-            <div className="analytics-cont-inner">
-
-                <div className="class-overview-cont">
-                    <div className="class-report-numbers">
-                        <div syle={{display:'flex',flexDirection:'row',flex:1,width:'"100%',justifyContent:"space-between",alignItems:'center'}}>
-                    <p className="overview-title">Overview</p>
- <select className="subject-filter">
-                        <option>Filter</option>
-                           
-                        </select>
-                        </div>
-                    <div className="overview-report-cont1">
-                      <p className="overview-report-title">Currently Enrolled</p>
-                    </div>
-                    <div className="overview-report-cont2">
-                    <p className="overview-report-title">Marked Present</p>
-                        </div>
-                        <div className="overview-report-cont3">
-                        <p className="overview-report-title">Active Students</p>
-                        
-                        </div>
-                    </div>
-                    <div className="class-report-graph">
-                        <div className="class-report-nav">
-                    <p className="graph-title" >Interaction Analytics</p>
-                    <MoreVertIcon fontSize="small"/>
-
-                    </div>
-                    <div className="doughnut-cont">
-               <Doughnut
-          data={state}
-          options={{
-            title:{
-              display:true,
-              text:'Average Rainfall per month',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}/>
+      <div className="analytics-cont-inner">
+        <div className="class-overview-cont">
+          <div className="class-report-numbers">
+            <div
+              syle={{
+                display: "flex",
+                flexDirection: "row",
+                flex: 1,
+                width: '"100%',
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <p className="overview-title">Overview</p>
+              <select className="subject-filter">
+                <option>Filter</option>
+              </select>
+            </div>
+            <div className="overview-report-cont1">
+              <p className="overview-report-title">Currently Enrolled</p>
+            </div>
+            <div className="overview-report-cont2">
+              <p className="overview-report-title">Marked Present</p>
+            </div>
+            <div className="overview-report-cont3">
+              <p className="overview-report-title">Active Students</p>
+            </div>
           </div>
-                    </div>
-                    
-                </div>
-                <div className="class-report-cont">
-                <div className="class-report-cont-inner">
-                    <div className="class-attendance-report">
-                        <div className="class-attendance-nav">
-                        <p className="class-attendance-graph-title">Attendance Analysis</p>
-                        <select className="class-activity-timespan">
-                        <option>Filter</option>
-                            <option>Yearly</option>
-                            <option>Monthly</option>
-                            <option>Daily</option>
-                        </select>
-                        
-                        </div>
-                        <div className="bar-cont">
-
-                        <div className="bar-cont2">
-                       
-
-</div>
-</div>
-                    </div>
-                    <div className="class-activity-report">
-                    <div className="class-attendance-nav">
-                        <p className="class-activity-title">Class Activity</p>
-                        <select className="class-activity-timespan">
-                            <option>present</option>
-                            <option>last 24hr</option>
-                        </select>
-
-                        </div>
-                        
-                        <div className="class-activity-nav2">
-                            <p>Name</p>
-                            <p>Role</p>
-                            <p>Attendance</p>
-                            <p>Last Active</p>
-                            <p>Activity</p>
-
-                        </div>
-                        <div className="class-activity-content">
-                            <p><i>No Data Available</i></p>
-
-                        </div>
-                        </div>
-
-                </div>
-                    
-                    
-                    </div>
-            
-
+          <div className="class-report-graph">
+            <div className="class-report-nav">
+              <p className="graph-title">Interaction Analytics</p>
+              <MoreVertIcon fontSize="small" />
             </div>
-
+            <div className="doughnut-cont">
+              <Doughnut
+                data={state}
+                options={{
+                  title: {
+                    display: true,
+                    text: "Average Rainfall per month",
+                    fontSize: 20,
+                  },
+                  legend: {
+                    display: true,
+                    position: "right",
+                  },
+                }}
+              />
             </div>
-    )
+          </div>
+        </div>
+        <div className="class-report-cont">
+          <div className="class-report-cont-inner">
+            <div className="class-attendance-report">
+              <div className="class-attendance-nav">
+                <p className="class-attendance-graph-title">
+                  Attendance Analysis
+                </p>
+                <select className="class-activity-timespan">
+                  <option>Filter</option>
+                  <option>Yearly</option>
+                  <option>Monthly</option>
+                  <option>Daily</option>
+                </select>
+              </div>
+              <div className="bar-cont">
+                <div className="bar-cont2"></div>
+              </div>
+            </div>
+            <div className="class-activity-report">
+              <div className="class-attendance-nav">
+                <p className="class-activity-title">Class Activity</p>
+                <select className="class-activity-timespan">
+                  <option>present</option>
+                  <option>last 24hr</option>
+                </select>
+              </div>
+
+              <div className="class-activity-nav2">
+                <p>Name</p>
+                <p>Role</p>
+                <p>Attendance</p>
+                <p>Last Active</p>
+                <p>Activity</p>
+              </div>
+              <div className="class-activity-content">
+                <p>
+                  <i>No Data Available</i>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Content/index.js
+++ src/Content/index.js
@@ -1,42 +1,57 @@
-import React ,{useEffect}from "react"
-import "./style.css"
-import ContentHeader from "../ContentHeader"
-import { AnalyticsRounded,Timer,Graph,QuestionAnswer, AccountCircle,AccountTree, BookmarkAdd, Analytics, Bookmark, NotificationsActiveRounded, CloseOutlined,AddOutlined, School } from "@mui/icons-material"
-import { Tooltip } from "@mui/material"
+import React, { useEffect } from "react";
+import "./style.css";
+import ContentHeader from "../ContentHeader";
+import {
+  AnalyticsRounded,
+  Timer,
+  Graph,
+  QuestionAnswer,
+  AccountCircle,
+  AccountTree,
+  BookmarkAdd,
+  Analytics,
+  Bookmark,
+  NotificationsActiveRounded,
+  CloseOutlined,
+  AddOutlined,
+  School,
+} from "@mui/icons-material";
+import { Tooltip } from "@mui/material";
 import GaugeChart from "react-gauge-chart";
-import toast from "react-hot-toast"
-import Notifications from "../Notifications"
-import  Calendar from "react-calendar"
-import Addcont from "../Addcont"
-import {useParams} from "react-router-dom"
-import 'react-calendar/dist/Calendar.css'
-import Weeklyreport from "../Weeklyreport"
-import {selectUser,login,logout} from "../features/userSlice"
-import Reminder from "../Reminder"
-import Todayreport from "../Todayreport"
-import axios from "axios"
-import {useSelector} from "react-redux"
-import Sidebarprofile from "../Sidebarprofile"
+import toast from "react-hot-toast";
+import Notifications from "../Notifications";
+import Calendar from "react-calendar";
+import Addcont from "../Addcont";
+import { useParams } from "react-router-dom";
+import "react-calendar/dist/Calendar.css";
+import Weeklyreport from "../Weeklyreport";
+import { selectUser, login, logout } from "../features/userSlice";
+import Reminder from "../Reminder";
+import Todayreport from "../Todayreport";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import Sidebarprofile from "../Sidebarprofile";
 
-export default function Content({setSlide,notifications,setNotifications, sidebarprofile,setSidebarprofile}){
-    const [value,onChange] = React.useState(new Date())
-  
-    const user= useSelector(selectUser)
-    const [calendarbtn,setCalendarbtn] = React.useState(false)
-    const [timetablebtn,setTimetablebtn] = React.useState(false)
-    const [addbtn,setAddbtn] = React.useState(true)
-    const [workspacedata,setWorkspacedata] =  React.useState([])
-   
-    
-    const config ={
-        headers:{"x-auth-token":localStorage.getItem('token')}
-    }
-   
+export default function Content({
+  setSlide,
+  notifications,
+  setNotifications,
+  sidebarprofile,
+  setSidebarprofile,
+}) {
+  const [value, onChange] = React.useState(new Date());
 
-   
+  const user = useSelector(selectUser);
+  const [calendarbtn, setCalendarbtn] = React.useState(false);
+  const [timetablebtn, setTimetablebtn] = React.useState(false);
+  const [addbtn, setAddbtn] = React.useState(true);
+  const [workspacedata, setWorkspacedata] = React.useState([]);
 
-   
-    /**<div onClick={()=>{
+  const config = {
+    headers: { "x-auth-token": localStorage.getItem("token") },
+  };
+
+  /**<div onClick={()=>{
                     setSlide(true)}} className="join-mtng">
 
  <VideoCall></VideoCall>
@@ -46,79 +61,177 @@ export default function Content({setSlide,notifications,setNotifications, sideba
                 <div  className="ask-qstn">
                     <QuestionAnswer></QuestionAnswer>
                     <p>ask question</p></div> */
-    return(
-    
-        <div className="content">
-          
-           <ContentHeader  sidebarprofile={sidebarprofile} setSidebarprofile={setSidebarprofile} setNotifications={setNotifications}/>
-             {sidebarprofile && <Sidebarprofile sidebarprofile={sidebarprofile} setSidebarprofile={setSidebarprofile}></Sidebarprofile> }           
+  return (
+    <div className="content">
+      <ContentHeader
+        sidebarprofile={sidebarprofile}
+        setSidebarprofile={setSidebarprofile}
+        setNotifications={setNotifications}
+      />
+      {sidebarprofile && (
+        <Sidebarprofile
+          sidebarprofile={sidebarprofile}
+          setSidebarprofile={setSidebarprofile}
+        ></Sidebarprofile>
+      )}
 
-             <div>
-                
-            <div style={{display:"flex",width:"95%",padding:7}}>
-                <p style={{fontSize:14,fontWeight:'600',color:"gray"}}>Overview</p>
-
-            </div>
-            <div style={{display:"flex",flexDirection:'row',width:"55%",justifyContent:'space-between',padding:5}}>
-            <button onClick={()=>{
-                setTimetablebtn(true)
-                setCalendarbtn(false)
-            }} style={{padding:7,borderColor:'gray',border:timetablebtn?"white":"1px solid #eee",borderRadius:15,justifyContent:'center',backgroundColor:timetablebtn?"rgb(122,122,243)":'white',alignItems:'center',alignContent:'center',width:"120px",cursor:'pointer'}}>
+      <div>
+        <div style={{ display: "flex", width: "95%", padding: 7 }}>
+          <p style={{ fontSize: 14, fontWeight: "600", color: "gray" }}>
+            Overview
+          </p>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "55%",
+            justifyContent: "space-between",
+            padding: 5,
+          }}
+        >
+          <button
+            onClick={() => {
+              setTimetablebtn(true);
+              setCalendarbtn(false);
+            }}
+            style={{
+              padding: 7,
+              borderColor: "gray",
+              border: timetablebtn ? "white" : "1px solid #eee",
+              borderRadius: 15,
+              justifyContent: "center",
+              backgroundColor: timetablebtn ? "rgb(122,122,243)" : "white",
+              alignItems: "center",
+              alignContent: "center",
+              width: "120px",
+              cursor: "pointer",
+            }}
+          >
             Timetable
-            </button>
-            <button onClick={()=>{
-                setCalendarbtn(true)
-                setTimetablebtn(false)
-            }}  name="Calendar" style={{padding:7,borderColor:calendarbtn?'white':'gray',border:calendarbtn?"0px solid white":"1px solid #eee",borderRadius:15,justifyContent:'center',backgroundColor:calendarbtn?'rgb(122, 122, 243)':'white',alignItems:'center',alignContent:'center',width:"120px",cursor:'pointer'}}>
+          </button>
+          <button
+            onClick={() => {
+              setCalendarbtn(true);
+              setTimetablebtn(false);
+            }}
+            name="Calendar"
+            style={{
+              padding: 7,
+              borderColor: calendarbtn ? "white" : "gray",
+              border: calendarbtn ? "0px solid white" : "1px solid #eee",
+              borderRadius: 15,
+              justifyContent: "center",
+              backgroundColor: calendarbtn ? "rgb(122, 122, 243)" : "white",
+              alignItems: "center",
+              alignContent: "center",
+              width: "120px",
+              cursor: "pointer",
+            }}
+          >
             Calendar
-            </button>
-           
-            <button style={{padding:7,borderColor:'gray',border:"1px solid #eee",borderRadius:15,justifyContent:'center',backgroundColor:'white',alignItems:'center',alignContent:'center',width:"120px",cursor:'pointer'}}>
+          </button>
+
+          <button
+            style={{
+              padding: 7,
+              borderColor: "gray",
+              border: "1px solid #eee",
+              borderRadius: 15,
+              justifyContent: "center",
+              backgroundColor: "white",
+              alignItems: "center",
+              alignContent: "center",
+              width: "120px",
+              cursor: "pointer",
+            }}
+          >
             Classrooms
-            </button>
-             
+          </button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            width: "90%",
+            justifyContent: "space-between",
+            padding: 10,
+            flexDirection: "row",
+          }}
+        >
+          <Weeklyreport />
+          <Reminder />
+          <div
+            style={{
+              backgroundColor: "rgb(122, 122, 243)",
+              position: "absolute",
+              bottom: "0px",
+              height: "10px",
+              left: "0px",
+              width: "100%",
+            }}
+          ></div>
+          <div
+            style={{
+              backgroundColor: "rgb(122, 122, 243)",
+              boxShadow: "rgba(10, 10, 10, 0.2) 0px 2px 8px 0px",
+              alignItems: "center",
+              cursor: "pointer",
+              display: "flex",
+              justifyContent: "center",
+              position: "absolute",
+              zIndex: "3",
+              bottom: "50px",
+              borderRadius: "50%",
+              height: "50px",
+              right: "40px",
+              width: "50px",
+            }}
+          >
+            {addbtn ? (
+              <AddOutlined
+                onClick={() => {
+                  setAddbtn(false);
+                }}
+                style={{ color: "white" }}
+              ></AddOutlined>
+            ) : (
+              <CloseOutlined style={{ color: "white" }}></CloseOutlined>
+            )}
+          </div>
 
-          
+          {!addbtn && <Addcont addbtn={addbtn} setAddbtn={setAddbtn}></Addcont>}
+          <div className="weekly-report-cont">
+            <div className="analytics-icon-cont">
+              <Bookmark
+                style={{ color: "rgb(122, 122, 243)", fontSize: 19 }}
+              ></Bookmark>
+
+              <p
+                style={{
+                  fontSize: 13,
+                  fontWeight: "590",
+                  color: "rgb(122, 122, 243)",
+                }}
+              >
+                Pending events
+              </p>
             </div>
-            <div style={{display:'flex',width:'90%',justifyContent:'space-between',padding:10,flexDirection:'row'}}>
 
-         <Weeklyreport/>
-         <Reminder/>
-            <div style={{backgroundColor:'rgb(122, 122, 243)',position:'absolute',bottom:"0px",height:"10px",left:'0px',width:"100%"}}></div>
-<div style={{backgroundColor:'rgb(122, 122, 243)', boxShadow:"rgba(10, 10, 10, 0.2) 0px 2px 8px 0px",alignItems:'center',cursor:"pointer",display:'flex',justifyContent:'center',position:'absolute',bottom:"50px",borderRadius:"50%",height:"50px",right:'40px',width:"50px"}}>
-    {addbtn?<AddOutlined onClick={()=>{
-        setAddbtn(false)
-    }} style={{color:'white'}}>
-
-    </AddOutlined>:<CloseOutlined style={{color:'white'}}></CloseOutlined>}
-</div>
-
-     {!addbtn&&<Addcont addbtn={addbtn} setAddbtn={setAddbtn}></Addcont>}   
-            <div className="weekly-report-cont">
-                    <div className="analytics-icon-cont">
-                   <Bookmark style={{color:'rgb(122, 122, 243)',fontSize:19}}></Bookmark>
-
-                   <p style={{fontSize:13,fontWeight:"590",color:'rgb(122, 122, 243)'}}>Pending events</p>                             
-
-                    </div>
-
-<p style={{fontSize:13}}>There are no events</p>
+            <p style={{ fontSize: 13 }}>There are no events</p>
+          </div>
         </div>
-
-             
-             </div>
-             
-             
-        </div>
-        <div style={{display:"flex",width:"90%",flexDirection:'column',padding:10}}>
-             <div style={{display:"flex",width:"100%"}}>
-               
-             </div>
-<Todayreport/>
-             </div>
-            
-        </div>
-    )
+      </div>
+      <div
+        style={{
+          display: "flex",
+          width: "90%",
+          flexDirection: "column",
+          padding: 10,
+        }}
+      >
+        <div style={{ display: "flex", width: "100%" }}></div>
+        <Todayreport />
+      </div>
+    </div>
+  );
 }
-
-
